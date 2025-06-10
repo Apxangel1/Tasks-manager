85d6891da14a3d37c5e214c330a2118a0f296b24
Volodymyr Vitsenko <your@mail.com>
2025-06-10 18:12:10 +0200

    Feat: Add models.py and write models.
    - Every user is a worker, and will have a position in the team.
    - Every team is uniquely named, and has optional fields: description, projects, members. Additionally, a team could have multiple projects, and vice versa.
    - Every project is uniquely named, and has optional fields: description and teams.
    - Every task will have: name, completion status, unique task type and priority. Optionaly, tasks might have: description, deadline and a list of assignees. Priority is one of 4 options: low, medium, high, urgent.
    
    Note: Teams relate to it's projects with a string "Project" since that class is defined below it.
    Other solution would be to use "annotations" from "__future__".

---
+++ Tasks-manager/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, User
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    projects = models.ManyToManyField("Project", related_name="teams", blank=True)
+    members = models.ManyToManyField(Worker, related_name="teams", blank=True)
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    teams = models.ManyToManyField(Team, related_name='projects', blank=True)
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    deadline = models.DateTimeField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="tasks", blank=True)
+
+    class Priority(models.TextChoices):
+        low = "Low"
+        medium = "Medium"
+        high = "High"
+        urgent = "Urgent"
+
+    priority = models.CharField(
+        max_length=6,
+        choices=Priority,
+        default=Priority.medium
+    )
+
+    class Meta:
+        ordering = ["name"]
