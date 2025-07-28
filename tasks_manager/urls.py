960dd43897e649dd473efe53a1c426f2db374de6
Volodymyr Vitsenko <your@mail.com>
2025-07-28 22:11:08 +0200

    feat: added Mytasks page

--- tasks_manager/urls.py
+++ tasks_manager/urls.py
@@ -1,14 +1,38 @@
-from .views import TasksListView, index, ProfileView, PasswordChange, SelfRegisterView
+from .views import (
+    TasksListView,
+    index,
+    ProfileView,
+    PasswordChange,
+    SelfRegisterView,
+    ProjectCreateView,
+    ProjectListView,
+    ProjectDetailView,
+    TaskCreateView,
+    PasswordChangeDone,
+    CreateTaskTypeView,
+    TaskDetailView,
+    AddAssigneesView,
+    MyTasksView
+)
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path("", index, name="index" ),
-    path("projects/<int:pk>/tasks/", TasksListView.as_view(), name="tasks"),
+    path("projects/create/", ProjectCreateView.as_view(), name="project_create"),
+    path("projects/", ProjectListView.as_view(), name="projects"),
+    path("project/<int:project_pk>/", ProjectDetailView.as_view(), name="project"),
+    path("projects/<int:project_pk>/tasks/", TasksListView.as_view(), name="tasks"),
+    path("projects/<int:project_pk>/tasks/create", TaskCreateView.as_view(), name="create_task"),
+    path("projects/<int:project_pk>/tasks/<int:task_pk>/", TaskDetailView.as_view(), name="task_details"),
+    path('create_task_type', CreateTaskTypeView.as_view(), name='create_task_type'),
+    path('tasks/<int:task_pk>/add-assignees/', AddAssigneesView.as_view(), name='add_assignees'),
+    path('tasks/my_tasks/', MyTasksView.as_view(), name='my_tasks'),
     path("login/", auth_views.LoginView.as_view(template_name='authentication/sign-in.html'), name='signin'),
     path("password_reset/", auth_views.PasswordResetView.as_view(template_name='authentication/reset-password.html'), name='password_reset'),
     path("register/", SelfRegisterView.as_view(), name="signup"),
     path("profile", ProfileView.as_view(), name="profile"),
     path("change_password", PasswordChange.as_view(), name="change_password"),
+    path("change_password_done", PasswordChangeDone.as_view(), name="change_password_done"),
 ]
--- tasks_manager/views.py
+++ tasks_manager/views.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import PasswordChangeView
-from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views import generic
-from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_POST
+from django.views import generic, View
+from django.http import JsonResponse
 
 from .forms import WorkerForm, ProfileEditForm, ProjectForm, TaskForm
-from .models import Task, Worker, Project, Organization, Position
+from .models import Task, Worker, Project, Team, Position, TaskType
 
 
 @login_required
@@ -28,7 +31,7 @@ def index(request):
 
     return render(request, "manager/index.html", context=context)
 
-class SidebarProjectsView(LoginRequiredMixin, TemplateView):
+class SidebarProjectsView(LoginRequiredMixin, generic.TemplateView):
     template_name = "manager/includes/sidebar.html"
 
     def get_context_data(self, **kwargs):
@@ -56,59 +59,124 @@ class ProfileView(LoginRequiredMixin, generic.UpdateView):
 class PasswordChange(PasswordChangeView):
     model = Worker
     template_name = "authentication/password-change.html"
-    success_url = "authentication/password-change-done.html"
+    success_url = reverse_lazy("change_password_done")
+
+class PasswordChangeDone(generic.TemplateView):
+    model = Worker
+    template_name = "authentication/password-change-done.html"
+    success_url = reverse_lazy("index")
 
 class SelfRegisterView(generic.CreateView):
     form_class = WorkerForm
     template_name = "authentication/sign-up.html"
-    success_url = "index"
+    success_url = reverse_lazy("index")
 
     def form_valid(self, form):
         user = form.save(commit=False)
 
-        org_name = form.cleaned_data.get("organization_name")
-        org = Organization.objects.create(name=org_name.strip())
+        team_name = form.cleaned_data.get("team_name")
+        team = Team.objects.create(name=team_name.strip())
+        user.team = team
 
-        owner_pos, _ = Position.objects.get_or_create(name="Owner")
+        creator_team, _ = Position.objects.get_or_create(
+            name="Creator",
+            team=team
+        )
 
-        user.organization = org
-        user.position = owner_pos
+        user.position = creator_team
         user.save()
+        login(self.request, user)
         return redirect(self.success_url)
 
 class ProjectCreateView(generic.CreateView):
     model = Project
     form_class = ProjectForm
     template_name = "manager/project/create.html"
-    success_url = "projects/"
+    success_url = reverse_lazy("projects")
+
+    def form_valid(self, form):
+        form.instance.team = self.request.user.team
+        return super().form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["team"] = self.request.user.team
+        return kwargs
 
 class ProjectListView(generic.ListView):
     model = Project
     context_object_name = "projects_list"
     template_name = "manager/project/list.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
-
 class ProjectDetailView(generic.DetailView):
     model = Project
     template_name = "manager/project/detail.html"
+
+    def get_object(self, queryset=None):
+        project_pk = self.kwargs.get('project_pk')
+        return get_object_or_404(Project, pk=project_pk)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["task_list"] = Task.objects.filter(project=self.object)
+        return context
 
 class TaskCreateView(generic.CreateView):
     model = Task
     template_name = "manager/tasks/create.html"
     form_class = TaskForm
-    success_url = "projects/"
+    success_url = reverse_lazy("projects")
 
     def form_valid(self, form):
-        form.instance.org = self.request.user.org
-        form.instance.project = Project.objects.get(pk=self.kwargs['project_id'])
+        form.instance.team = self.request.user.team
+        form.instance.project = Project.objects.get(pk=self.kwargs['pk'])
         form.instance.is_completed = False
 
         return super().form_valid(form)
 
 class TasksListView(LoginRequiredMixin, generic.ListView):
     model = Task
-    context_object_name = "tasks_list"
+    context_object_name = "task_list"
     template_name = "manager/tasks/list.html"
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+    template_name = "manager/tasks/detail.html"
+
+    def get_object(self, queryset=None):
+        task_pk = self.kwargs.get('task_pk')
+        return get_object_or_404(Task, pk=task_pk)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["workers_list"] = Worker.objects.filter(team=self.request.user.team)
+        return context
+
+class CreateTaskTypeView(View):
+    def post(self, request):
+        name = request.POST.get('objectName')
+        team = request.user.team
+        if name:
+            category = TaskType.objects.create(name=name, team=team)
+            return JsonResponse({'id': category.id, 'name': category.name}, status=200)
+        return JsonResponse({'error': 'Name is required'}, status=400)
+
+@method_decorator(require_POST, name='dispatch')
+class AddAssigneesView(View):
+    def post(self, request, task_pk):
+        task = get_object_or_404(Task, pk=task_pk)
+        assignee_ids = request.POST.getlist('assignees[]')
+        workers = Worker.objects.filter(pk__in=assignee_ids)
+
+        task.assignees.add(*workers)
+
+        return JsonResponse({'success': True, 'added': [w.pk for w in workers]})
+
+class MyTasksView(generic.ListView):
+    model = Task
+    template_name = "manager/tasks/list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tasks_list"] = Task.objects.filter(assignees=self.request.user)
+        return context
